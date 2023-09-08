ae8e62d21f0bb18050099d6ba7320c1dba9ce25f
2354970 <143269060+2354970@users.noreply.github.com>
2023-09-08 16:33:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main () {
+    bool condition= true;
+    int n=0, primo=1, secondo=1;
+    cin>>n;
+    while(secondo<=n){
+    if (condition) {
+        condition = false;
+        cout << secondo << endl;
+    } else {
+        cout << secondo << endl;
+            int terzo = secondo;
+            secondo += primo;
+            primo = terzo;
+        }
+    }
+    return 0;
 }
